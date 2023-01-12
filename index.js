e71fd754d5a1ccabf0a71f2a70a0d8d0c55ec94d
20B-055-SE <20B-055-SE@uit.edu>
2023-01-12 16:55:04 +0500

    added views

---
+++ index.js
@@ -0,0 +1,41 @@
+const app = require("express")()
+const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+const ejs = require("ejs");
+
+const vehicle = require("./models/vehicles")
+
+dotenv.config()
+mongoose.connect(process.env.URI + "/Vehicle")
+    .then(res => {
+        console.log("Connected to MongoDB server")
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set("view engine", "ejs")
+
+
+
+//routes
+
+app.post("/save",async(req,res)=>{
+    const newVehicle = await vehicle.create(req.body)
+    await newVehicle.save()
+    res.send("Added!")
+})
+app.get("/", async(req, res) => {
+    const vehicles = await vehicle.find();
+    res.render("index",{
+        vehicles:vehicles
+    })
+})
+
+
+//server
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT}`)
+})
